876fbedb9b21f3036673189be4fa387111651259
oabanin-WORK <juristaba@gmail.com>
2020-08-18 16:16:38 +0400

    JQUERY: add .fixed to .header on scroll

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
-$(function(){
-    $(window).on("scroll", function(event){
-        const intro = $('#intro').innerHeight();
-        console.log(intro);
+
+;(function($, undefined){
+    $(function(){
+
+
+        /* Header Fixed */
+
+        const header =$('#header');
+        const introH = $('#intro').innerHeight();
+        let scrollOffset = $(window).scrollTop();
+        
+        function checkFixedHeaderMenu(scrollOffset){
+
+            let hasClass = header.hasClass('fixed');
+
+            if(!hasClass && scrollOffset>introH){
+                header.addClass("fixed");
+            }
+
+            if(hasClass && scrollOffset<introH){
+                header.removeClass("fixed");
+            }
+        }
+
+    	checkFixedHeaderMenu(scrollOffset);
+
+        $(window).on("scroll", function(event){
+            scrollOffset = $(this).scrollTop();
+            checkFixedHeaderMenu(scrollOffset);
+        });
+        
     });
-});
+})(jQuery);
